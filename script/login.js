1717b65386c5e93ffcaa31e511ae556f29485b82
Najmul-Huda70 <nh5696080@gmail.com>
2026-02-19 09:10:04 +0600

    55%

--- script/login.js
+++ script/login.js
@@ -4,7 +4,7 @@ document.getElementById("login-btn").addEventListener("click", function () {
   const pin = document.getElementById("pin-input").value;
   if (mobileNo === "01712345678" && pin === "1234") {
     alert("login successfully!");
-    window.location.assign("/home.html");
+    window.location.assign("./home.html");
   } else {
     alert("login failed!");
     return;
